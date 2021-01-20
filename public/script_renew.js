8ed912af7ca93e9c9715136da95e5a3b2a21a2a3
matsumoto@iflytek.co.jp <matsumoto@iflytek.co.jp>
2021-01-20 14:31:01 +0900

    bf

--- public/script_renew.js
+++ public/script_renew.js
@@ -1088,7 +1088,8 @@ function showLinkWindow(msg) {
   */
   const root = document.location.href;
   const url =
-    root + "?room_name=" + msg.room_name + "&password=" + msg.password;
+    // root + "?room_name=" + msg.room_name + "&password=" + msg.password;
+    root + "?secret=" + $("#room_id").val()
   Swal.fire({
     title: "招待リンク",
     // icon: "info",
