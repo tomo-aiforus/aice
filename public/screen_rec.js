e867b67bf4543c8f1b6926f1299113e982dada36
matsumoto@iflytek.co.jp <matsumoto@iflytek.co.jp>
2021-03-22 21:16:38 +0900

    t

--- public/screen_rec.js
+++ public/screen_rec.js
@@ -114,7 +114,7 @@ function download(){
   const a = document.createElement("a");
   a.style.display = "none";
   a.href = url;
-  a.download = "rec.webm";
+  a.download = "screen-capture-video.webm";
   document.body.appendChild(a);
   a.click();
   setTimeout(() => {
